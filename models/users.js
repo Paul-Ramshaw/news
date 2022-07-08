6af7f168c2b7287a55fd21e8b1f4ddcaabb9f4c5
Paul-Ramshaw <ramshaw@hotmail.co.uk>
2022-07-08 12:23:02 +0100

    add endpoint to get user by username

--- models/users.js
+++ models/users.js
@@ -6,6 +6,21 @@ exports.selectUsers = () => {
   });
 };
 
+exports.selectUser = (username) => {
+  return db
+    .query(
+      `SELECT * FROM users
+       WHERE username = $1;`,
+      [username]
+    )
+    .then(({ rows }) => {
+      if (!rows[0]) {
+        return Promise.reject({ status: 404, msg: 'The user does not exist' });
+      }
+      return rows[0];
+    });
+};
+
 exports.checkUserExists = (username) => {
   if (!username) {
     return Promise.reject({
